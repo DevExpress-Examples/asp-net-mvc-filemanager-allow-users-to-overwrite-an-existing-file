7aa2127865413b7969f05b0c126fa14f69ebeb2d
SvetlanaMikheeva <80316217+SvetlanaMikheeva@users.noreply.github.com>
2023-11-30 11:17:18 +0400

    Update AccountModels.cs

--- CS/S37186_MVC/Models/AccountModels.cs
+++ CS/S37186_MVC/Models/AccountModels.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Data.Entity;
 using System.Globalization;
 using System.Web.Mvc;
 using System.Web.Security;
